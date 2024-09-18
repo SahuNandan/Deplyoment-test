f669b01cce4c8ef0e0d326f5de9d080d224a0804
SahuNandan <nandansahu755@gmail.com>
2024-09-18 15:14:17 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hey! <code>Nandan Sahu</code> I am an Engineer
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Follow me Now
         </a>
       </header>
     </div>
